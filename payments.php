c8a785744cbe991ebfb5c0720d1db949a86b6ca1
jamelcharouel <jamel.charouel@gmail.com>
2015-11-14 01:17:58 -0500

    Simple blockchain.info login form and QR code generator implemented,

--- payments.php
+++ payments.php
@@ -6,15 +6,15 @@ $Blockchain = new \Blockchain\Blockchain();
 $identifier = $_POST['identifier'];
 $password = stripslashes($_POST['password']);
 
-var_dump($identifier);
-print_r($password) . "<br />" . PHP_EOL;
+//var_dump($identifier);
+//print_r($password) . "<br />" . PHP_EOL;
 
 if(is_null($identifier) || is_null($password)) {
     echo "Please enter a wallet GUID and password.<br/>";
     exit;
 }
 
-//https://blockchain.info/qr?data=1JcnG7mrpj3gcE7njLmqxeGyQJMquCkHpR&size=250
+//https://blockchain.info/qr?data=1JcnG7mrpj3gcE7njLmqxeGyQJMquCkHpR&amount=500&size=250
 //Method for creating QR code from bitcoin address
 
 $guid="a5ae20b6-39c2-49a5-91bf-de21e99fa5f2"; //My identifier
@@ -23,20 +23,18 @@ $myaddress = "1JcnG7mrpj3gcE7njLmqxeGyQJMquCkHpR";
 
 //Get user wallet addresses, pick first one and give users that address
 $Blockchain->Wallet->credentials($identifier, $password);
-echo "Using wallet " . $Blockchain->Wallet->getIdentifier() . "<br />" . PHP_EOL;
+//echo "Using wallet " . $Blockchain->Wallet->getIdentifier() . "<br />" . PHP_EOL;
 $DDaddresses = $Blockchain->Wallet->getAddresses();
-print_r($DDaddresses[0]);
+//print_r($DDaddresses[0]);
 
 //My Address 1JcnG7mrpj3gcE7njLmqxeGyQJMquCkHpR
 
 $newAddress = json_decode(file_get_contents("https://blockchain.info/merchant/$identifier/new_address?password=$password"), true);
 $parseAddress = $newAddress['address'];
 
-echo "New Address: " . $parseAddress;
+//echo "New Address: " . $parseAddress;
 
-$bitcoinpayaddress = PHP_EOL . "https://blockchain.info/qr?data=$parseAddress&size=250" . PHP_EOL;
-echo "Send your riders to this address to get paid!: " . $bitcoinpayaddress;
-
+$bitcoinpayaddress = "https://blockchain.info/qr?data=$parseAddress&size=250";
 
 $json_url = "https://blockchain.info/merchant/$identifier/balance?password=$password";
 
@@ -46,7 +44,7 @@ $json_feed = json_decode($json_data);
 
 $balance = $json_feed->balance;
 
-echo "Your balance is " . $balance;
+//echo "Your balance is " . $balance;
 
 ?>
 
@@ -55,14 +53,15 @@ echo "Your balance is " . $balance;
 <script type="text/javascript" src="https://blockchain.info/Resources/wallet/pay-now-button.js"></script>
 <body>
 
-<form input="text" 
-
-<p> Please send your tip to this BTC address: <?php echo $parseAddress; ?> </p> <b> <img src="https://blockchain.info/qr?data=<?php echo $parseAddress; ?>&size=250" > </b> 
+<p> Direct your friends to this link to get paid!: <a href = "<?php echo $bitcoinpayaddress; ?>" > Blockchain Link </a> </p>  Or have them send you money from the QR code here: <br>  <b> <img src="<?php echo $bitcoinpayaddress; ?>" > </b> 
 
 
+<!--
+
+This code is just for a bitcoin donate button
 
 <div style="font-size:16px;margin:0 auto;width:300px" class="blockchain-btn"
-     data-address="<?php echo $parseAddress ?>" 	
+     data-address="<?php echo $parseAddress; ?>" 	
      data-shared="false">
     <div class="blockchain stage-begin">
         <img src="https://blockchain.info/Resources/buttons/donate_64.png"/>
@@ -81,6 +80,7 @@ echo "Your balance is " . $balance;
         <font color="red">[[error]]</font>
     </div>
 </div>
+-->
 
 
 </body>
