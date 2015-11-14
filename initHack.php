a42a7c3e4a2d24dc7ab05cb815e59bf8d1dbb890
Leah E. Procopi <leah.procopi@gmail.com>
2015-11-14 12:41:49 -0500

    updated two things

--- initHack.php
+++ initHack.php
@@ -15,7 +15,10 @@ if($db->connect_error){
     exit;
 }
 
-$makeUsers = $db->query("create table Users(ID int primary key not null auto_increment, Name char(50) not null, Username char (50) not null, Email char(100) not null, Password char(15) not null)");
+$drop = $db->query("drop table Users");
+mysql_query($drop);
+
+$makeUsers = $db->query("create table Users(ID int primary key not null auto_increment, Name char(50) not null, Username char (50) not null, Email char(100) not null, Password char(15) not null, imgdata longblob not null)");
 $makeRelationship = $db->query("create table Relationship(Friend1 int not null, Friend2 int not null, Status int not null)");
 
 
