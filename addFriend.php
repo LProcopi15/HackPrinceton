8a739c2b84f48821bb80ea21be48bffb9ede9090
Molly Davey <mmd4dg@virginia.edu>
2015-11-14 17:04:33 -0500

    merge conflict resolution

---
+++ addFriend.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form action = "friendRequest.php" method = POST>
+    Add a friend ya bish <br></br>
+        <input type = text placeholder = "Enter friend's user name" name = "friendUsername" ><br></br>
+        <input type = submit name = submit >
+</form>
+    
+
+    </body>
+</html>
--- userCreationSQL.php
+++ userCreationSQL.php
@@ -1,7 +1,24 @@
 <?php
 
+<<<<<<< HEAD
 //create database:
 $db = mysqli_connect('localhost', 'root', 'password', 'test');
+=======
+/*define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "test");
+
+
+
+//Create new database named test
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+$testdatabase = mysql_select_db("test");
+*/
+
+$db = mysqli_connect('localhost', 'root', 'password', 'test');
+
+>>>>>>> origin/master
 if($db->connect_error){
     print "Error - Could not connnect to MySQL";
     exit;
@@ -21,8 +38,7 @@ if ($_POST['facebook_name'] != null) {
 
 		$db->query($query) or die ("Invalid insert " .$db->error);
 
-	echo 'Facebook Sign In';
-
+	header("Location: landingPage.html");
 }
 else{
 	if ($_POST['password1']==$_POST['password2']) {
@@ -36,11 +52,13 @@ else{
 		$query = "INSERT INTO Users(`Name`, `Username`, `Email`, `Password`, `imgdata`) VALUES('$name', '$username', '$email', '$password', '$picture')";
 
 		$db->query($query) or die ("Invalid insert " .$db->error);
+
 	}
 	else {
 		echo "Passwords do not match. Please go back to login";
 	}
-	echo 'Regular Sign in';
+	header("Location: landingPage.html");
+	//echo 'Regular Sign in';
 }
 
 ?>
