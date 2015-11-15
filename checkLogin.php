da8f8e8f0d9bb5513f349ca6b24fad08a73621c8
Molly Davey <mmd4dg@virginia.edu>
2015-11-14 20:13:30 -0500

    linked sign up and login pages to index

--- checkLogin.php
+++ checkLogin.php
@@ -4,21 +4,27 @@ session_start();
 $sessionUsername = $_POST['username'];
 $sessionPassword = $_POST['password'];
 //$db = mysqli_connect('localhost', 'root', 'password', 'test');
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "test");
+//define("DB_HOST", "localhost");
+//define("DB_USER", "root");
+//define("DB_PASSWORD", "");
+//define("DB_DATABASE", "test");
 
 
 
 //Create new database named test
-$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-$testdatabase = mysql_select_db("test");
+//$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+//$testdatabase = mysql_select_db("test");
 
+//if($db->connect_error){
+//    print "Error - Could not connnect to MySQL";
+//    exit;
+//}
+$db = mysqli_connect('localhost', 'root', 'password', 'test');
 if($db->connect_error){
     print "Error - Could not connnect to MySQL";
     exit;
 }
+
 $result = mysqli_query($db,"SELECT * FROM Users WHERE Username = '$sessionUsername' AND password = '$sessionPassword'");    
 if(!$result) {
 	die("you suck: " . mysql_error());
@@ -31,7 +37,6 @@ if(!$result) {
      $_SESSION["ID"]=$row['ID'];
      $_SESSION["Email"]=$row['Email'];
      
-     header("Location: /HackPrinceton/addFriend.php");
      $_SESSION["login"]= "true";
      header("Location: views/index.html");
         }
